f4155a80b9370bf4d3d21239f61b1ec72e169490
AbrahamLugoRamirez <abraham.lugo@integrait.co>
2021-07-16 19:33:46 -0500

    ajuste

--- src/App.java
+++ src/App.java
@@ -4,24 +4,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Scanner sc = new Scanner(System.in);
-
         int cant = sc.nextInt();
         sc.nextLine();
-
         for (int i = 0; i < cant; i++) {            
             System.out.println("Ingrese datos");
             String datos = sc.nextLine();
-            String vector[] = datos.split(" ");
-            
+            String vector[] = datos.split(" ");            
             for (int j = 0; j < vector.length; j++) {
-                System.out.println(vector[j]);
-                
+                System.out.println(vector[j]);                
             }
-
         }
-
-
-
-
     }
 }
